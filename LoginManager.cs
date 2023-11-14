a38ed10b618a7cba7fe36caca0f92b95248d86cb
calle fransson <calle-fransson@hotmail.com>
2023-11-14 09:51:41 +0100

    Add project files.

---
+++ LoginManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginÖvining
+{
+    public class LoginManager
+    {
+        private List<User> _users;
+        public LoginManager()
+        {
+            _users = new List<User>();
+
+            _users.Add(new User("admin", "123"));
+            _users.Add(new User("bankowner", "123"));
+            _users.Add(new User("user", "123"));
+        }
+
+        public bool Login(string username,string password)
+        {
+                foreach (User user in _users)
+                {
+                    if (user.Username == username && user.Password == password)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            
+           
+        }
+    }
+}
